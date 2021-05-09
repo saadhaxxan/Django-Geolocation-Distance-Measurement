27b21fecac8a3fe80009166a2fb07a4edcf7d531
Saad Hassan <saad.haxxan786@gmail.com>
2021-05-09 14:20:37 +0500

    added folium and distance measurement

--- measurements/views.py
+++ measurements/views.py
@@ -4,17 +4,20 @@ from .forms import MeasurementModelForm
 from geopy import Photon
 from geopy.distance import geodesic
 from .utils import get_geo
+import folium
 
 def calculate_distance_view(request):
     # obj = get_object_or_404(Measurement,id=1)
     form = MeasurementModelForm(request.POST or None)
     geolocater = Photon(user_agent='measurements')
 
-    ip = '72.14.207.99'
+    ip = '101.53.254.73'
     country,city,lat,lon = get_geo(ip)
     # print(country,lon,lat,city)
     location = geolocater.geocode(city,timeout=10, exactly_one=True)
     pointA = (lat,lon)
+    m = folium.Map( location=pointA,zoom_start=8)
+    folium.Marker([lat,lon],tooltip='Click here to learn more',popup=city['city'],icon=folium.Icon(color='blue')).add_to(m)
     if form.is_valid():
         instance = form.save(commit=False)
         destination_ = form.cleaned_data.get('destination')
@@ -26,9 +29,10 @@ def calculate_distance_view(request):
         instance.location = location
         instance.distance = distance
         instance.save()
+    m = m._repr_html_()
     context = {
         'form':form,
-        'distance':distance
+        'map':m
     }
     return render(request,'measurements/main.html',context)
 
